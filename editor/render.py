7e2b811b6594b606fb05c1316b82edfc5825c709
Ray Chen <tankimarshal2@gmail.com>
2021-07-02 15:49:45 +0100

    Fixed scene loading again

--- editor/render.py
+++ editor/render.py
@@ -1,56 +1,38 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from pyunity import SceneManager, config, Clock
-import sys
-import threading
-
-config.windowProvider = sys.modules[__name__]
-frame = None
-window = None
+from pyunity import config
+from pyunity.scenes import Scene
 
 class OpenGLFrame(QOpenGLWidget):
     SPACER = None
     def __init__(self):
         super(OpenGLFrame, self).__init__()
-        self.default = SceneManager.AddScene("Scene")
+        self.default = Scene("Default Scene")
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update)
+        self.scene = None
     
     def initializeGL(self):
-        global frame
-        frame = self
         self.default.Start()
     
     def paintGL(self):
-        if window is not None:
-            window.update_func()
+        if self.scene is not None:
+            self.scene.update()
         else:
             self.default.update()
     
     def resizeGL(self, width, height):
-        if window is not None:
-            window.resize(width, height)
+        if self.scene is not None:
+            self.scene.mainCamera.Resize(width, height)
         else:
             self.default.mainCamera.Resize(width, height)
         self.update()
     
     def stop(self):
-        window.timer.stop()
+        self.timer.stop()
+        self.scene = False
     
     def start_scene(self, scene):
-        SceneManager.LoadScene(scene)
-
-class Window:
-    def __init__(self, name, resize):
-        global window
-        window = self
-        self.resize = resize
-
-    def quit(self):
-        global window
-        del window
-
-    def start(self, update_func):
-        frame.makeCurrent()
-        self.update_func = update_func
-        frame.timer.start(1000 / config.fps)
+        self.scene = scene
+        scene.Start()
+        self.timer.start(1000 / config.fps)
