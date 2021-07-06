8b8acc7d8ac40d7e1699ee47c566fe7e4279383f
Ray Chen <tankimarshal2@gmail.com>
2021-07-06 10:20:43 +0100

    Image on splash screen

--- splash.py
+++ splash.py
@@ -2,6 +2,7 @@ import os
 import tkinter
 import time
 import threading
+from PIL import ImageTk, Image
 
 def main():
     from editor.app import Application
@@ -9,13 +10,27 @@ def main():
     app.start()
 
 def splash():
-    a = tkinter.Tk()
+    root = tkinter.Tk()
+    root.overrideredirect(1)
+    root.attributes("-topmost", True)
+
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = int((screen_width / 2) - (256 / 2))
+    y = int((screen_height / 2) - (256 / 2))
+    root.geometry("256x256+" + str(x) + "+" + str(y))
+
+    canvas = tkinter.Canvas(root, width=256, height=256,
+        bd=0, highlightthickness=0, relief='ridge')
+    canvas.pack()
+    img = ImageTk.PhotoImage(Image.open("../pyunity.png").resize((256, 256)))
+    canvas.create_image(0, 0, anchor=tkinter.NW, image=img)
     while True:
         if os.getenv("PYUNITY_EDITOR_LOADED") == "1":
             break
-        a.update()
+        root.update()
         time.sleep(0.2)
-    a.destroy()
+    root.destroy()
 
 def start(func, args=[], kwargs={}):
     t = threading.Thread(target=splash)
