aed2a1f8c827d2b88effa703a305ddc0e6354be5
Ray Chen <tankimarshal2@gmail.com>
2023-07-04 17:07:14 +0100

    Use pyunity.resources classes for editor assets

--- pyunity_editor/local.py
+++ pyunity_editor/local.py
@@ -1,41 +1,18 @@
 from pathlib import Path
-from datetime import datetime
-import os
-import zipfile
-import shutil
+from pyunity.resources import ZipAssetResolver, PackageAssetResolver
+
+directory = Path.home() / ".pyunity" / ".editor"
+if not directory.is_dir():
+    directory.mkdir(parents=True)
 
 package = Path(__file__).resolve().parent
-if not package.exists():
+if package.parent.name.endswith(".zip"):
     package = package.parent
     if not package.is_file():
         raise Exception("Cannot find egg file")
-    egg = True
+    resolver = ZipAssetResolver(directory, package, __package__)
 else:
-    egg = False
-directory = Path.home() / ".pyunity" / ".editor"
+    resolver = PackageAssetResolver(directory, package)
 
 def getPath(local):
-    dest = directory / local
-    if egg:
-        with zipfile.ZipFile(package) as zf:
-            src = (Path(__package__) / local).as_posix()
-            if src not in zf.namelist():
-                raise Exception(f"No resource at {package / src}")
-            if dest.exists():
-                ziptime = datetime(*zf.getinfo(src).date_time)
-                if ziptime.timestamp() != dest.stat().st_mtime:
-                    return dest
-            out = zf.extract(src, directory)
-            os.makedirs(dest.parent, exist_ok=True)
-            shutil.move(out, dest.parent)
-            shutil.rmtree(Path(out).parent)
-            return str(dest)
-    else:
-        src = package / local
-        if not src.exists():
-            raise Exception(f"No resource at {src}")
-        if dest.exists() and src.stat().st_mtime != dest.stat().st_mtime:
-            return dest
-        dest.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copy(src, dest)
-        return str(dest)
+    return str(resolver.getPath(local))
