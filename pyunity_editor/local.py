8aef0235a535276a955cd44bc326c369bce7d53e
Ray Chen <tankimarshal2@gmail.com>
2023-07-06 10:19:42 +0100

    Comments for the workaround code in local.py

--- pyunity_editor/local.py
+++ pyunity_editor/local.py
@@ -2,6 +2,13 @@ from pathlib import Path
 import importlib.util
 import sys
 
+# Problem: Need to import `pyunity.resources` for splash
+# image fetching, but that will load the entirety of
+# pyunity and take a long time.
+# Solution: Get the module of `pyunity` but don't execute
+# it. Import `pyunity.resources` and `pyunity.logger`
+# then reset and execute `pyunity` correctly later.
+
 packageSpec = importlib.util.find_spec("pyunity")
 
 def importModule(submodule):
@@ -14,16 +21,20 @@ def importModule(submodule):
     spec.loader.exec_module(module)
     return module
 
+# Import `pyunity.logger` into `pyunity.Logger` for
+# use by `pyunity.resources`
 logger = importModule("logger")
 sys.modules["pyunity.logger"] = logger
 sys.modules["pyunity.Logger"] = logger
 
+# Get module but don't execute `pyunity`
 pyunity = importlib.util.module_from_spec(packageSpec)
 sys.modules["pyunity"] = pyunity
 resources = importModule("resources")
 sys.modules["pyunity.resources"] = resources
 loaded = False
 
+# Code for asset resolver
 directory = Path.home() / ".pyunity" / ".editor"
 if not directory.is_dir():
     directory.mkdir(parents=True)
@@ -38,11 +49,14 @@ else:
     resolver = resources.PackageAssetResolver(directory, package)
 
 def getPath(local):
+    # Most Qt functions cannot take Path arguments
     return str(resolver.getPath(local))
 
 def fixPackage():
+    # Only load once
     global loaded
     if loaded:
         return
+    sys.modules.pop("pyunity.Logger")
     packageSpec.loader.exec_module(sys.modules["pyunity"])
     loaded = True
