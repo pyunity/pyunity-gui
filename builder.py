94b9696ca1ddcccfba9f8712d4fa229551904283
Ray Chen <tankimarshal2@gmail.com>
2023-07-09 16:04:32 +0100

    Separate entry script from builder script

--- builder.py
+++ builder.py
@@ -228,95 +228,7 @@ try:
             if file.endswith(".dll"):
                 shutil.copy(os.path.join(datafolder, file), ".")
 
-    print("WRITE pyunity-editor.c", flush=True)
-    with open("pyunity-editor.c", "w+") as f:
-        f.write(textwrap.dedent("""
-        #define PY_SSIZE_T_CLEAN
-        #define Py_LIMITED_API 0x03060000
-        #include <Python.h>
-
-        #ifdef NOCONSOLE
-        #include <windows.h>
-        #define CHECK_ERROR(n) if (PyErr_Occurred() != NULL) { showError(); exit(1); }
-
-        void showError() {
-            printf("Error encountered\n");
-            PyObject *type, *value, *traceback;
-            PyErr_Fetch(&type, &value, &traceback);
-            PyErr_Print();
-
-            PyObject *tracebackModule = PyImport_ImportModule("traceback");
-            PyObject *formatFunc = PyObject_GetAttrString(tracebackModule, "format_exception");
-            Py_DecRef(tracebackModule);
-
-            PyErr_NormalizeException(&type, &value, &traceback);
-            PyException_SetTraceback(value, traceback);
-
-            PyObject *lines = PyObject_CallFunctionObjArgs(formatFunc, value, NULL);
-            PyObject *sep = PyUnicode_FromString("");
-            PyObject *joined = PyUnicode_Join(sep, lines);
-            Py_DecRef(sep);
-            Py_DecRef(lines);
-
-            wchar_t *msg = PyUnicode_AsWideCharString(joined, NULL);
-            MessageBoxW(NULL, msg, L"Error loading PyUnity Editor", 0x10L);
-            PyMem_Free(msg);
-
-            PyErr_Restore(type, value, traceback);
-        }
-        #else
-        #define CHECK_ERROR() if (PyErr_Occurred() != NULL) { PyErr_Print(); exit(1); }
-        #endif
-
-        int main(int argc, char **argv) {
-            wchar_t *path = Py_DecodeLocale("Lib\\python.zip;Lib;Lib\\win32;Lib\\win32\\lib", NULL);
-            Py_SetPath(path);
-            wchar_t **program = (wchar_t**)PyMem_Malloc(sizeof(wchar_t**) * argc);
-            for (int i = 0; i < argc; i++) {
-                program[i] = Py_DecodeLocale(argv[i], NULL);
-            }
-            if (program[0] == NULL) {
-                fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
-                exit(1);
-            }
-            Py_SetProgramName(program[0]);
-            Py_Initialize();
-            PySys_SetArgvEx(argc, program, 0);
-            CHECK_ERROR();
-
-            PyObject *editor = PyImport_ImportModule("pyunity_editor.cli");
-            CHECK_ERROR();
-
-            #ifdef NOCONSOLE
-            PyObject *func = PyObject_GetAttrString(editor, "gui");
-            #else
-            PyObject *func = PyObject_GetAttrString(editor, "run");
-            #endif
-            CHECK_ERROR();
-
-            PyObject *res = PyObject_CallFunction(func, NULL);
-            CHECK_ERROR();
-
-            if (Py_FinalizeEx() < 0) {
-                exit(1);
-            }
-            for (int i = 0; i < argc; i++) {
-                PyMem_Free((void*)program[i]);
-            }
-            PyMem_Free((void*)program);
-            PyMem_Free((void*)path);
-            printf("Safely freed memory\n");
-            return 0;
-        }
-
-        #ifdef NOCONSOLE
-        int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                char* pCmdLine, int nShowCmd) {
-            return main(__argc, __argv);
-        }
-        #endif
-        """))
-
+    shutil.copy(orig + "\\standalone\\pyunity-editor.c", "..")
     shutil.copy(orig + "\\standalone\\icons.ico", "..")
     shutil.copy(orig + "\\standalone\\icons.rc", "..")
     shutil.copy(orig + "\\standalone\\version.rc", "..")
@@ -334,7 +246,7 @@ try:
         print("COMPILE pyunity-editor.exe", flush=True)
         subprocess.call([
             "cl.exe", "/nologo", "/O2", "/Wall",
-            "/Tcpyunity-editor.c", "/Fo..\\pyunity-editor.obj",
+            "/Tc..\\pyunity-editor.c", "/Fo..\\pyunity-editor.obj",
             f"/I{sys.base_prefix}\\include", "/DNOCONSOLE",
             "/link", "..\\icons.res", "..\\version.res", "/subsystem:windows",
             f"/libpath:{sys.base_prefix}\\libs",
@@ -355,10 +267,9 @@ try:
         print("COMPILE pyunity-editor.exe", flush=True)
         subprocess.call([
             "gcc.exe", "-O2", "-Wall", "-mwindows", "-DNOCONSOLE",
-            "-o", "pyunity-editor.exe", "pyunity-editor.c", "..\\icons.o", "..\\version.o",
+            "-o", "pyunity-editor.exe", "..\\pyunity-editor.c", "..\\icons.o", "..\\version.o",
             "-L.", f"-l{zipname}", f"-I{sys.base_prefix}\\include",
         ], stdout=sys.stdout, stderr=sys.stderr)
-    os.remove("pyunity-editor.c")
 
     print(f"ZIP pyunity-editor.zip", flush=True)
     os.chdir(tmp)
---
+++ standalone/pyunity-editor.c
@@ -0,0 +1,88 @@
+#define PY_SSIZE_T_CLEAN
+#define Py_LIMITED_API 0x03060000
+#include <Python.h>
+
+#ifdef NOCONSOLE
+#include <windows.h>
+#define CHECK_ERROR(n) if (PyErr_Occurred() != NULL) { showError(); exit(1); }
+
+void showError() {
+    printf("Error encountered\n");
+    PyObject *type, *value, *traceback;
+    PyErr_Fetch(&type, &value, &traceback);
+    PyErr_Print();
+
+    PyObject *tracebackModule = PyImport_ImportModule("traceback");
+    PyObject *formatFunc = PyObject_GetAttrString(tracebackModule, "format_exception");
+    Py_DecRef(tracebackModule);
+
+    PyErr_NormalizeException(&type, &value, &traceback);
+    PyException_SetTraceback(value, traceback);
+
+    PyObject *lines = PyObject_CallFunctionObjArgs(formatFunc, value, NULL);
+    PyObject *sep = PyUnicode_FromString("");
+    PyObject *joined = PyUnicode_Join(sep, lines);
+    Py_DecRef(sep);
+    Py_DecRef(lines);
+
+    wchar_t *msg = PyUnicode_AsWideCharString(joined, NULL);
+    MessageBoxW(NULL, msg, L"Error loading PyUnity Editor", 0x10L);
+    PyMem_Free(msg);
+
+    PyErr_Restore(type, value, traceback);
+}
+#else
+#define CHECK_ERROR() if (PyErr_Occurred() != NULL) { PyErr_Print(); exit(1); }
+#endif
+
+int main(int argc, char **argv) {
+    wchar_t *path = Py_DecodeLocale("Lib\\python.zip;Lib;Lib\\win32;Lib\\win32\\lib", NULL);
+    Py_SetPath(path);
+    wchar_t **program = (wchar_t**)PyMem_Malloc(sizeof(wchar_t**) * argc);
+    for (int i = 0; i < argc; i++) {
+        program[i] = Py_DecodeLocale(argv[i], NULL);
+    }
+    if (program[0] == NULL) {
+        #ifdef NOCONSOLE
+        MessageBoxW(NULL, L"Fatal error: cannot decode argv[0]", L"Error loading PyUnity Editor", 0x10L);
+        #else
+        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+        #endif
+        exit(1);
+    }
+    Py_SetProgramName(program[0]);
+    Py_Initialize();
+    PySys_SetArgvEx(argc, program, 0);
+    CHECK_ERROR();
+
+    PyObject *editor = PyImport_ImportModule("pyunity_editor.cli");
+    CHECK_ERROR();
+
+    #ifdef NOCONSOLE
+    PyObject *func = PyObject_GetAttrString(editor, "gui");
+    #else
+    PyObject *func = PyObject_GetAttrString(editor, "run");
+    #endif
+    CHECK_ERROR();
+
+    PyObject *res = PyObject_CallFunction(func, NULL);
+    CHECK_ERROR();
+
+    if (Py_FinalizeEx() < 0) {
+        exit(1);
+    }
+    for (int i = 0; i < argc; i++) {
+        PyMem_Free((void*)program[i]);
+    }
+    PyMem_Free((void*)program);
+    PyMem_Free((void*)path);
+    printf("Safely freed memory\n");
+    return 0;
+}
+
+#ifdef NOCONSOLE
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+        char* pCmdLine, int nShowCmd) {
+    return main(__argc, __argv);
+}
+#endif
